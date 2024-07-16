43a4c103c975a85bc6c67ac63f2406cb430ea674
VuTrungNghia <vunghia467@gmail.com>
2024-07-16 11:52:14 +0700

    UpdateAppmodule.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { User } from '../entity/user.entity';
+import { Room } from '../entity/room.entity';
+import { Message } from '../entity/message.entity';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nest_chat_app',
+      entities: [User, Room, Message],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User, Room, Message]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
