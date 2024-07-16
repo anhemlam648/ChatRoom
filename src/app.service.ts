8cab5d13f20b4f22271abaadf9970233e96606cc
VuTrungNghia <vunghia467@gmail.com>
2024-07-16 11:40:30 +0700

    CreateData

---
+++ src/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    const imageUrl = '/picture.jpg';
+    // setTimeout(()=>{
+    //   console.log('Async operation completed');
+    // },3000);
+    return `
+   <html>
+        <body>
+          <h1>Hello, and welcome to my chatroom! Foundation Vu Trung Nghia</h1>
+          <div style="text-align: center;">
+            <img src="${imageUrl}" alt="Image" style="margin: auto;">
+          </div>
+        </body>
+      </html>
+     `;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as path from 'path';
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);;
+  app.useStaticAssets(path.join(__dirname, '..', 'public')); 
+  await app.listen(3000);
+}
+bootstrap();
