4ec07ca07a20f80a2593d30040e4cd07aa77d8b7
VuTrungNghia <vunghia467@gmail.com>
2024-07-20 09:37:43 +0700

    createChat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 import { join } from 'path';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getHello(): string {
+  //   res.sendFile(join(__dirname, '..', 'public', './public/index.html'));
+  // }
   @Get()
-  getHello(): string {
+  getHello(@Res() res: Response): void {
     res.sendFile(join(__dirname, '..', 'public', 'index.html'));
   }
 }
