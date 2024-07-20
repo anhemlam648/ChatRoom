aa9c2db5aee4a96880f70900d6a3a9a5d10ed639
VuTrungNghia <vunghia467@gmail.com>
2024-07-20 10:12:14 +0700

    createChatTest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express';
 import { join } from 'path';
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const imageUrl = '/picture.jpg';
+    const imageUrl = '../public/picture.jpg';
     // setTimeout(()=>{
     //   console.log('Async operation completed');
     // },3000);
