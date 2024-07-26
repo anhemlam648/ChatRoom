179859b1419260c3c24b0826b2a77e03312fcd6b
VuTrungNghia <vunghia467@gmail.com>
2024-07-26 11:20:36 +0700

    updateNotificationService

--- src/notificationService/notification.service.ts
+++ src/notificationService/notification.service.ts
@@ -8,13 +8,16 @@ import { User } from '../../entity/user.entity';
 export class NotificationService {
   constructor(
     @InjectRepository(Notification)
-    private notificationRepository: Repository<Notification>,
+    private readonly notificationRepository: Repository<Notification>,
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+    private readonly userRepository: Repository<User>,
   ) {}
 
   async createNotification(userId: number, message: string): Promise<Notification> {
     const user = await this.userRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new Error('User not found');
+    }
     const notification = this.notificationRepository.create({ message, user });
     return this.notificationRepository.save(notification);
   }
@@ -25,6 +28,9 @@ export class NotificationService {
 
   async markAsRead(notificationId: number): Promise<Notification> {
     const notification = await this.notificationRepository.findOne({ where: { id: notificationId } });
+    if (!notification) {
+      throw new Error('Notification not found');
+    }
     notification.isRead = true;
     return this.notificationRepository.save(notification);
   }
