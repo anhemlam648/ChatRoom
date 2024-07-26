7f8638ef1f1653ee3deb71d604670f693108c14f
VuTrungNghia <vunghia467@gmail.com>
2024-07-26 11:22:42 +0700

    updateChatGateway

--- src/gateway/chat.gateway.ts
+++ src/gateway/chat.gateway.ts
@@ -1,13 +1,18 @@
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody,ConnectedSocket,} from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, MessageBody, ConnectedSocket } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessageService } from '../../src/messageService/message.service';
+import { NotificationService } from '../notificationService/notification.service';
 import { Message } from '../../entity/message.entity';
+
 @WebSocketGateway({ namespace: '/chat' })
 export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(private readonly messageService: MessageService) {}
+  constructor(
+    private readonly messageService: MessageService,
+    private readonly notificationService: NotificationService,
+  ) {}
 
   @SubscribeMessage('message')
   async handleMessage(
@@ -15,6 +20,11 @@ export class ChatGateway {
     @ConnectedSocket() client: Socket
   ): Promise<void> {
     const newMessage = await this.messageService.create(message);
+
+    if (message.receiverId) {
+      await this.notificationService.createNotification(message.receiverId, `New message from ${newMessage.user.username}: ${message.content}`);
+    }
+
     this.server.to(message.room.id.toString()).emit('message', newMessage);
   }
 
