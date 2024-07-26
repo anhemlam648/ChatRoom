5a812601199ebf503f3b24db4e30a587cc2d655d
VuTrungNghia <vunghia467@gmail.com>
2024-07-26 11:21:44 +0700

    updateMessageService

--- src/messageService/message.service.ts
+++ src/messageService/message.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Message } from '../../entity/message.entity';
+import { NotificationService } from '../notificationService/notification.service';
+import { User } from '../../entity/user.entity';
 
 @Injectable()
 export class MessageService {
   constructor(
     @InjectRepository(Message)
     private readonly messageRepository: Repository<Message>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly notificationService: NotificationService,
   ) {}
 
   findAll(): Promise<Message[]> {
@@ -15,17 +20,27 @@ export class MessageService {
   }
 
   findOne(id: number): Promise<Message | undefined> {
-    return this.messageRepository.findOneBy({ id });
+    return this.messageRepository.findOne({ where: { id } });
   }
 
-  create(message: Partial<Message>): Promise<Message> {
+  async create(message: Partial<Message>): Promise<Message> {
     const newMessage = this.messageRepository.create(message);
-    return this.messageRepository.save(newMessage);
+    const savedMessage = await this.messageRepository.save(newMessage);
+
+    // create notification send user
+    if (message.receiverId) {
+      const receiver = await this.userRepository.findOne({ where: { id: message.receiverId } });
+      if (receiver) {
+        await this.notificationService.createNotification(receiver.id, `New message: ${message.content}`);
+      }
+    }
+
+    return savedMessage;
   }
 
   async update(id: number, message: Partial<Message>): Promise<Message | undefined> {
     await this.messageRepository.update(id, message);
-    return this.messageRepository.findOneBy({ id });
+    return this.messageRepository.findOne({ where: { id } });
   }
 
   async remove(id: number): Promise<void> {
